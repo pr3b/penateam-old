6c67915ca88045d0edd971fdf8760d424afcf99b
Arista Indrajaya <arista.indrajaya@gmail.com>
2023-05-16 12:47:44 +0700

    implement css on react app

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+import "../styles/Hero.css"
+import "../styles/About.css"
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
