aa4c713bce591023b652f70752f84b7f7a2496ec
Arista Indrajaya <arista.indrajaya@gmail.com>
2023-05-16 12:48:09 +0700

    Update hero component

--- pages/components/Hero.js
+++ pages/components/Hero.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Image from "next/image";
 
 function Hero() {
   return (
     <div id="home">
-      <div className="home-container">
-        <h1 className="home-title">Combining Technology and Processes</h1>
+      <div className="hero">
+        <div class="text-hero">
+          <h1>Your go-to documentation partner</h1>
+          <p>Quickly scale your technical documentation with our affordable subscription service.</p>
+          <a href="#" class="cta-button">Work with us</a>
+          <p>Trusted by 30+ companies across the globe.</p>
+        </div>
+        <div class="image-hero">
+          {/* <Image src="path/to/your-image.jpg" alt="Hero Image" width={300} height={200}/> */}
+        </div>
       </div>
     </div>
   );
