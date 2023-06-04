221fa274b8214fd9381113a34b9d61cef4396c41
Arista Indrajaya <arista.indrajaya@gmail.com>
2023-06-05 05:23:00 +0700

    implement to floating nav

--- pages/components/FloatingNav.js
+++ pages/components/FloatingNav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import FloatingNavAuth from "./FloatingNavAuth";
 
 function FloatingNav() {
   return (
@@ -19,6 +20,7 @@ function FloatingNav() {
             <a href="\#pricing" className="floating-nav__link w-nav-link">
               Pricing
             </a>
+            <FloatingNavAuth />
           </nav>
           {/* <div className="w-nav-button">
             <div className="w-icon-nav-menu"></div>
