f81f322d270c7755cb7912eb762bd7f9db221933
Arista Indrajaya <arista.indrajaya@gmail.com>
2023-06-07 10:54:00 +0700

    navigation add subs page

--- pages/components/FloatingNavAuth.js
+++ pages/components/FloatingNavAuth.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { useSession, signIn, signOut } from 'next-auth/react'
+import Link from 'next/link';
 
 function FloatingNavAuth() {
   const {data: session} = useSession();
 
   if(session){
     return(
-      <>
-        Signed as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
+      <> 
+        <a href="#" class="floating-nav__link w-nav-link">
+          Signed as {session.user.email} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </a>
+        <Link href="/my-subscriptions" class="floating-nav__link w-nav-link">
+            My Subscriptions
+        </Link>
       </>
     )
   }
