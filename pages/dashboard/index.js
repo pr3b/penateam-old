3b7d19424fd47c7e4b55e1a32d839ef8e2d1ec62
Arista Indrajaya <arista.indrajaya@gmail.com>
2023-06-09 17:33:21 +0700

    implement cursor on dashboard page

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import DashboardMain from './DashboardMain'
 import DashboardSidebar from './DashboardSidebar'
+import CustomCursor from '../components/CustomCursor'
+import CursorSVG from "../../public/assets/images/icons/cursor-pena-01.svg"
 
 const Dashboard = () => {
   return (
     <div className="h-screen flex overflow-hidden bg-gray-100" id="body-container">
+      <CustomCursor customCursor={CursorSVG} />
       <DashboardSidebar />
       <DashboardMain />
     </div>
