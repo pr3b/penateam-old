2ef7607d4e02810e59abb161c519fc6d33a172c4
Arista Indrajaya <arista.indrajaya@gmail.com>
2023-06-09 09:26:00 +0700

    Add sidebar component

---
+++ pages/dashboard/DashboardSidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SidebarContent from './SidebarContent'
+
+const DashboardSidebar = () => {
+  return (
+    <div className="hidden md:flex md:flex-shrink-0">
+      <SidebarContent />
+    </div>
+  )
+}
+
+export default DashboardSidebar
