e7b61e73bd5fa432f248647db0a7d43f0bd806c0
Arista Indrajaya <arista.indrajaya@gmail.com>
2023-06-14 09:37:00 +0700

    Implement userDropdown menu

--- pages/dashboard/DashboardMainNav.js
+++ pages/dashboard/DashboardMainNav.js
@@ -1,8 +1,7 @@
 import React from 'react'
+import UserDropdown from './components/UserDropdown';
 
 const DashboardMainNav = () => {
-  const open = false; 
-
   return (
     <div className="relative flex-shrink-0 flex h-16 bg-white shadow-sm">
       <div className="flex-1 px-4 flex justify-between">
@@ -51,77 +50,7 @@ const DashboardMainNav = () => {
           </div>
 
           {/* User Component */}
-          <div
-            // onClick={closeDropdown}
-            className="ml-3 relative"
-            // x-data={{ open: false }}
-          >
-            <div>
-              <button
-                // onClick={toggleDropdown}
-                className="max-w-xs mr-6 flex items-center text-sm focus:outline-none focus:shadow-outline"
-              >
-                <span className="inline-block h-6 w-6 rounded-full overflow-hidden bg-gray-100">
-                  <svg
-                    className="h-full w-full text-gray-300"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"
-                    />
-                  </svg>
-                </span>
-                <span className="px-3 font-medium text-sm text-gray-500">
-                  Cahyo Subroto
-                </span>
-                <svg
-                  className="fill-current text-gray-600"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="11.254"
-                  height="7"
-                  viewBox="0 0 11.254 7"
-                >
-                  <g id="pO66u9.tif" transform="translate(11.254 7) rotate(180)">
-                    <g id="Group_1812" transform="translate(0 0)">
-                      <path
-                        id="Path_5641"
-                        d="M5.625,0,0,5.621,1.367,7,5.639,2.719,9.9,6.988l1.356-1.36Z"
-                      />
-                      <path
-                        id="Path_5642"
-                        d="M5.625,0l5.628,5.627L9.9,6.988,5.639,2.719,1.367,7,0,5.621Z"
-                      />
-                    </g>
-                  </g>
-                </svg>
-              </button>
-            </div>
-            {open && (
-              <div className="origin-top-right absolute top-0 right-0 mt-2 w-48 rounded-md shadow-lg">
-                <div className="py-1 bg-white rounded-md shadow-xs">
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Account Settings
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Support
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Sign Out
-                  </a>
-                </div>
-              </div>
-            )}
-          </div>
+          <UserDropdown />
         </div>
       </div>
     </div>
