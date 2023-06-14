49258f77cd4165291ae7d83a7db85447bcc588c7
Arista Indrajaya <arista.indrajaya@gmail.com>
2023-06-14 09:52:00 +0700

    Create form implementation to add task

--- pages/dashboard/components/AddTaskForm.js
+++ pages/dashboard/components/AddTaskForm.js
@@ -1,11 +1,37 @@
 import React, { useState } from 'react';
+import { categoryDone, categoryInProgress, categoryRequest } from '@/utils/category';
+import { useSession } from 'next-auth/react';
+import axios from 'axios';
 
 const AddTaskForm = ({ onAddTask }) => {
   const [taskTitle, setTaskTitle] = useState('');
+  const {data: session, status} = useSession();
+  const userEmailNow = session?.user?.email
 
   const handleInputChange = (e) => {
     setTaskTitle(e.target.value);
   };
+
+  //Send email notification
+  const sendEmailNotificationUserCreateRequest = async(taskTitle, userEmail) => {
+    const emailData = {
+      to: userEmailNow,
+      from: "hello@penateam.com",
+      subject: "Penateam Task Update",
+      text: `New Task ${taskTitle} from ${userEmail}`,
+    };
+
+    try {
+      const res = await axios.post("/api/mail/email-notification", emailData);
+      if(res.status){
+        console.log("Email notification create Request sent successfully")
+      } else {
+        console.error("Failed to send email")
+      }
+    } catch (error) {
+      console.error("Error sending email:", error);
+    }
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,11 +47,12 @@ const AddTaskForm = ({ onAddTask }) => {
     const newTask = {
       id: taskId,
       title: taskTitle,
-      category: 'todo',
+      category: categoryRequest,
     };
 
     // Pass the new task to the parent component
     onAddTask(newTask);
+    sendEmailNotificationUserCreateRequest(taskTitle, userEmailNow)
 
     // Reset the form input
     setTaskTitle('');
