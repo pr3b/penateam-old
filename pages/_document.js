3210f2d4ba4f0ca787e7900b622f89f786281a46
Arista Indrajaya <arista.indrajaya@gmail.com>
2023-08-02 22:26:47 +0700

    implement head script braintree

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Include the Braintree Drop-in UI script here */}
+        <script async src="https://js.braintreegateway.com/web/dropin/1.39.0/js/dropin.min.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
