eb290705d980412f6380f2a3b728fb22f3b83952
Arista Indrajaya <arista.indrajaya@gmail.com>
2023-08-07 10:56:17 +0700

    fix date

--- utils/midtrans.js
+++ utils/midtrans.js
@@ -1,6 +1,6 @@
-export function MonthlySubscribtionObject(snapToken, amount, order_id = "",paymentType = 'credit_card', customerDetails = {}, description = '') {
+export function MonthlySubscribtionObject(snapToken, amount, order_id = "",paymentType = 'credit_card', customerDetails = { "first_name": "John", "email": "johndoe@email.com", "last_name": "Doe", "phone": "+62812345678"  }, description = '') {
   const date = new Date();
-  date.setFullYear(date.getFullYear() + 1);
+  date.setDate(date.getDate() + 1);
   const FullYear = date.getFullYear();
   const Month = (date.getMonth() + 1) < 9 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
   const DateNow = (date.getDate() < 9 ? '0' + date.getDate() : date.getDate());
@@ -9,7 +9,7 @@ export function MonthlySubscribtionObject(snapToken, amount, order_id = "",payme
   const Seconds = (date.getSeconds() < 9 ? '0' + date.getSeconds() : date.getSeconds());
 
   return {
-      "name": "MONTHLY_2019",
+      "name": "PENA_MONTHLY_SUBSCRIPTION",
       "amount": amount,
       "currency": "IDR",
       "payment_type": paymentType,
