1249a44e65082754b961e0d068d8fe7bcaa3723f
Arista Indrajaya <arista.indrajaya@gmail.com>
2023-08-07 18:42:48 +0700

    implement form validation email and phone

--- pages/customer.js
+++ pages/customer.js
@@ -21,6 +21,8 @@ const CustomerDetailForm = () => {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [phoneError, setPhoneError] = useState("");
   const [chooseLoading, setChooseLoading] = useState(null);
 
   const itemDetail = {
@@ -30,13 +32,27 @@ const CustomerDetailForm = () => {
     name: name,
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission logic here
-    console.log(firstName, lastName, email, phone);
-  };
-
   const handleProductClickMidtrans = async (subsObject, itemDetail, buttonId) => {
+    if (!firstName || !lastName || !email || !phone) {
+      return; // Abort if any required field is empty
+    }
+
+    // Validate email format
+    if (!isEmailValid(email)) {
+      setEmailError("Please enter a valid email address");
+      return;
+    } else {
+      setEmailError("");
+    }
+
+    // Validate phone number format
+    if (!isPhoneNumberValid(phone)) {
+      setPhoneError("Please enter a valid phone number");
+      return;
+    } else {
+      setPhoneError("");
+    }
+
     setChooseLoading(buttonId);
     try {
       const today = new Date();
@@ -88,6 +104,43 @@ const CustomerDetailForm = () => {
       setChooseLoading(null); // Set loading state back to false
     }, 3000); // Simulating a 2-second delay
   }
+
+  const isEmailValid = (email) => {
+    // Simple email validation using regular expression
+    // You can customize this validation as per your requirements
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return emailRegex.test(email);
+  };
+
+  const isPhoneNumberValid = (phone) => {
+    // Simple phone number validation using regular expression
+    const phoneRegex = /^\d{10,}$/;
+    return phoneRegex.test(phone);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === "email") {
+      setEmail(value);
+      if (!isEmailValid(value)) {
+        setEmailError("Please enter a valid email address");
+      } else {
+        setEmailError("");
+      }
+    } else if (name === "phone") {
+      setPhone(value);
+      if (!isPhoneNumberValid(value)) {
+        setPhoneError("Please enter a valid phone number");
+      } else {
+        setPhoneError("");
+      }
+    } else if (name === "firstName") {
+      setFirstName(value);
+    } else if (name === "lastName") {
+      setLastName(value);
+    }
+  };
 
   return (
   <>
@@ -167,11 +220,12 @@ const CustomerDetailForm = () => {
               First Name
             </label>
             <input
+              name="firstName" // Add this
               type="text"
               id="firstName"
               className="mt-1 p-2 w-full border rounded-md shadow-sm focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
               value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
+              onChange={handleInputChange}
             />
           </div>
 
@@ -181,47 +235,76 @@ const CustomerDetailForm = () => {
               Last Name
             </label>
             <input
+              name="lastName" // Add this
               type="text"
               id="lastName"
               className="mt-1 p-2 w-full border rounded-md shadow-sm focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
               value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
+              onChange={handleInputChange}
             />
           </div>
 
           {/* Email */}
           <div className="mb-4">
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
               Email
             </label>
             <input
+              name="email" // Add this
               type="email"
               id="email"
-              className="mt-1 p-2 w-full border rounded-md shadow-sm focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
+              className={`mt-1 p-2 w-full border rounded-md shadow-sm focus:ring-primary-500 focus:border-primary-500 sm:text-sm ${
+                emailError ? "border-red-500" : ""
+              }`}
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={handleInputChange}
             />
+            {emailError && (
+              <p className="text-red-500 text-sm mt-2">{emailError}</p>
+            )}
           </div>
 
           {/* Phone */}
           <div className="mb-6">
-            <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="phone"
+              className="block text-sm font-medium text-gray-700"
+            >
               Phone
             </label>
             <input
+              name="phone" // Add this
               type="text"
               id="phone"
-              className="mt-1 p-2 w-full border rounded-md shadow-sm focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
+              className={`mt-1 p-2 w-full border rounded-md shadow-sm focus:ring-primary-500 focus:border-primary-500 sm:text-sm ${
+                phoneError ? "border-red-500" : ""
+              }`}
               value={phone}
-              onChange={(e) => setPhone(e.target.value)}
+              onChange={handleInputChange}
             />
+            {phoneError && (
+              <p className="text-red-500 text-sm mt-2">{phoneError}</p>
+            )}
           </div>
 
           {/* Submit button */}
           <button
             className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-500 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
-            onClick={() => handleProductClickMidtrans(MonthlySubscribtionObject, itemDetail, "button")}
-            disabled={chooseLoading === "button"}
+            onClick={() => 
+              handleProductClickMidtrans(MonthlySubscribtionObject, 
+              itemDetail, 
+              "button"
+            )}
+            disabled={
+              !firstName || 
+              !lastName || 
+              !email || 
+              !phone || 
+              !(isEmailValid(email) && isPhoneNumberValid(phone)) ||
+              chooseLoading === "button"}
           >
             {chooseLoading === "button" ? (
               <LoadingPlaceholder />
