05a30f8e7a122aa57eb28ea8591c93b926b90f48
Arista Indrajaya <arista.indrajaya@gmail.com>
2023-08-09 14:19:16 +0700

    create midtrans success page

---
+++ pages/MidtransSuccess.js
@@ -0,0 +1,16 @@
+import React, {useEffect, useState, useCallback } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import LoadingComponent from './components/utils/Loading';
+
+const MidtransSuccess = () => {
+    // const router = useRouter();
+    // const [loading, setLoading] = useState(true);
+
+
+    // if (loading) {
+    //     return <LoadingComponent string={"Payment"} status={"processing"}/>;
+    // }
+};
+
+export default MidtransSuccess;
